413d62a3eb75ff484149254cd1cce367a98b7c68
Jamal .S <me@shrjamal.com>
2022-05-03 13:09:44 +0000

    add skills section

---
+++ src/lib/helpers/skills.ts
@@ -0,0 +1,146 @@
+export const skills = [
+  {
+    name: 'Frontend Developer',
+    techs: [
+      {
+        name: 'HTML5',
+        level: 'Advanced',
+      },
+      {
+        name: 'CSS3',
+        level: 'Intermediate',
+      },
+      {
+        name: 'Javascript',
+        level: 'Advanced',
+      },
+      {
+        name: 'Typescript',
+        level: 'Intemediate',
+      },
+      {
+        name: 'Reactjs',
+        level: 'Advanced',
+      },
+      {
+        name: 'Nextjs',
+        level: 'Advanced',
+      },
+      {
+        name: 'Vuejs',
+        level: 'Intermediate',
+      },
+      {
+        name: 'Svelte',
+        level: 'Intermediate',
+      },
+      {
+        name: 'TailwindCSS',
+        level: 'Intermediate',
+      },
+      {
+        name: 'Bootstrap',
+        level: 'Intermediate',
+      },
+    ],
+  },
+
+  {
+    name: 'Backend Developer',
+    techs: [
+      {
+        name: 'Nodejs',
+        level: 'Advanced',
+      },
+      {
+        name: 'Express',
+        level: 'Advanced',
+      },
+      {
+        name: 'Nestjs',
+        level: 'Advanced',
+      },
+      {
+        name: 'Graphql API',
+        level: 'Advanced',
+      },
+      {
+        name: 'MongoDB',
+        level: 'Intermediate',
+      },
+      {
+        name: 'MySQL',
+        level: 'Intermediate',
+      },
+      {
+        name: 'PostgresSQL',
+        level: 'Intermediate',
+      },
+    ],
+  },
+
+  {
+    name: 'Mobile Development',
+    techs: [
+      {
+        name: 'Flutter & Dart',
+        level: 'Advanced',
+      },
+      {
+        name: 'Android (Kotlin, Java)',
+        level: 'Basics',
+      },
+      {
+        name: 'iOS (Swift)',
+        level: 'Basics',
+      },
+    ],
+  },
+
+  // {
+  //   name: 'Tools',
+  //   techs: [
+  //     {
+  //       name: 'Git: Github, Gitlab, Bitbucket',
+  //       level: 'Advanced',
+  //     },
+  //     {
+  //       name: 'Trello, Asana',
+  //       level: 'Advanced',
+  //     },
+  //     {
+  //       name: 'VS Code',
+  //       level: 'Advanced',
+  //     },
+  //     {
+  //       name: 'Android Studio',
+  //       level: 'Intermediate',
+  //     },
+  //     {
+  //       name: 'Postman, Insomnia',
+  //       level: 'Intermediate',
+  //     },
+  //   ],
+  // },
+  // {
+  //   name: 'DevOps',
+  //   techs: [
+  //     {
+  //       name: 'Docker & Kubernetes',
+  //       level: 'Basics',
+  //     },
+  //     {
+  //       name: 'AWS',
+  //       level: 'Basics',
+  //     },
+  //     {
+  //       name: 'Google Cloud',
+  //       level: 'Basics',
+  //     },
+  //     {
+  //       name: 'Digital Ocean',
+  //       level: 'Basics',
+  //     },
+  //   ],
+  // },
+]
---
+++ src/lib/sections/Skills.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { skills } from '$lib/helpers/skills'
+</script>
+
+<section id="skills" class="mt-10 w-screen flex flex-col items-center">
+  <h2 class="text-primary text-xl">
+    Some Technologies I have experience with:
+  </h2>
+
+  <div
+    class="pt-4 px-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center "
+  >
+    {#each skills as s}
+      <div class="py-6 px-4 rounded-[1.25rem] w-[20rem] bg-base-300">
+        <h3 class="mb-6 font-bold text-center text-xl">
+          {s.name}
+        </h3>
+        <div class="grid grid-cols-2">
+          {#each s.techs as tech}
+            <div class="flex">
+              <i class="bx bxs-badge-check  text-primary" />
+              <div class="ml-2">
+                <h3 class="leading-4">{tech.name}</h3>
+                <!-- <span class="text-2xs">{tech.level}</span> -->
+              </div>
+            </div>
+          {/each}
+        </div>
+      </div>
+    {/each}
+  </div>
+</section>
