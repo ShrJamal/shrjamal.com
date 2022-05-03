23d10a6c4eb700529f5eac77ca31ca2719b1de0f
Jamal .S <me@shrjamal.com>
2022-05-03 15:18:42 +0000

    add GithubActivity comp

---
+++ src/lib/sections/GithubActivity.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { browser } from '$app/env'
+  import GitHubCalendar from 'github-calendar'
+  import 'github-calendar/dist/github-calendar.css'
+  browser &&
+    GitHubCalendar('.calendar', 'shrjamal', {
+      responsive: true,
+    })
+</script>
+
+<section id="github-activity" class="mt-6 flex flex-col items-center">
+  <h2 class="text-xl text-primary mb-2">Github Activity</h2>
+  <div class="calendar" />
+</section>
