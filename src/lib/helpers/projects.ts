6fbe27f741be1248699ca51a3e0419010aa1ee47
Jamal .S <me@shrjamal.com>
2022-05-21 12:45:19 +0100

    add projects components

---
+++ src/lib/helpers/projects.ts
@@ -0,0 +1,48 @@
+export const allProjects = [
+  {
+    type: 'Web',
+    projects: [
+      {
+        name: 'Proshop',
+        link: 'https://github.com/SHRJamal/proshop',
+        demo: 'https://shr-proshop.herokuapp.com/',
+        img: '/proshop.png',
+      },
+      {
+        name: 'Linkdap',
+        link: 'private',
+        demo: 'https://linkdap.fly.dev',
+        img: '/linkdap.png',
+      },
+      {
+        name: 'Covider Tracker',
+        link: 'https://github.com/SHRJamal/covid-tracker',
+        demo: 'covid-tracker.shrjamal.com',
+        img: '/covid-tracker.png',
+      },
+    ],
+  },
+
+  {
+    type: 'Mobile',
+    projects: [
+      {
+        name: 'Flutter Social',
+        link: 'https://codecanyon.net/item/flutterfire-social-messenger/25605604',
+        demo: 'https://drive.google.com/file/d/12LiNnsdDSmkSUurWoWBIFQ4Jy1MI1Jr5/view',
+        img: '/flutter-social.png',
+      },
+    ],
+  },
+  {
+    type: 'Scripts',
+    projects: [
+      {
+        name: 'Crypto Trader',
+        link: 'private',
+        demo: 'private',
+        img: '/trader.png',
+      },
+    ],
+  },
+]
---
+++ src/lib/sections/Projects.svelte
@@ -0,0 +1,62 @@
+<script>
+  import { allProjects } from '$lib/helpers/projects'
+
+  let category = 'Web'
+  const categories = allProjects.map((v) => v.type)
+  $: items = allProjects.find((v) => v.type === category)?.projects || []
+</script>
+
+<section class="work section" id="work">
+  <span class="section__subtitle">Featured Projects </span>
+  <h2 class="section__title">Some Things I've built</h2>
+
+  <div class="flex justify-center items-center  mb-8">
+    {#each categories as c}
+      <span
+        class="cursor-pointer py-1 px-3 rounded-lg"
+        class:active-work={c === category}
+        data-filter={c}
+        on:click={() => (category = c)}
+      >
+        {c}
+      </span>
+    {/each}
+  </div>
+
+  <div class="pt-4 container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+    {#each items as p}
+      <div
+        class="p-4 rounded-2xl "
+        style="background-color: var(--container-color)"
+      >
+        <div class="mb-3 mx-auto h-[15rem]">
+          <img class="rounded w-full h-full" src={p.img} alt={p.name} />
+        </div>
+        <h3 class="mb-1">{p.name}</h3>
+
+        <a
+          class="w-max flex items-center gap-x-1 text-primary"
+          href={p.demo}
+          target="_blank"
+        >
+          Demo <i
+            class="bx bx-right-arrow-alt text-lg transition-transform duration-1000 translate-x-20"
+          />
+        </a>
+      </div>
+    {/each}
+  </div>
+</section>
+
+<style>
+  .work__icon {
+    font-size: 1rem;
+    transition: 0.4s;
+  }
+
+  /* Active item work*/
+  .active-work {
+    background-color: var(--first-color);
+    color: var(--body-color);
+  }
+</style>
