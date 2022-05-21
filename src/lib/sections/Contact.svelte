2cf6b2e06627b766d504f3a3c05f1dee21d16570
Jamal .S <me@shrjamal.com>
2022-05-21 12:45:33 +0100

    add contact comp

---
+++ src/lib/sections/Contact.svelte
@@ -0,0 +1,66 @@
+<section class="text-center" id="contact">
+  <h2 class="text-2xl text-primary mb-2">Get in Touch</h2>
+
+  <div class="flex justify-center items-center space-x-2">
+    <a
+      class="btn btn-circle bg-base-100"
+      href="https://twitter.com/shrjamal"
+      target="_blank"
+    >
+      <i class="text-blue-500 text-xl bx bxl-twitter" />
+    </a>
+    <a
+      class="btn btn-circle bg-base-100  "
+      href="https://www.linkedin.com/in/shrjamal"
+      target="_blank"
+    >
+      <i class="text-blue-300 text-xl bx bxl-linkedin-square" />
+    </a>
+    <a
+      class="btn btn-circle bg-base-100"
+      href="https://github.com/shrjamal"
+      target="_blank"
+    >
+      <i class="text-white text-xl bx bxl-github" />
+    </a>
+    <!-- Email -->
+    <div class="bg-base-200 rounded-xl p-4">
+      <div class="flex flex-col items-center">
+        <h3 class="text-lg">
+          <i class="bx bx-mail-send " /> Email
+        </h3>
+        <span>me@shrjamal.com</span>
+        <a
+          class="underline text-primary"
+          href="mailto:me@shrjamal.com"
+          target="_blank"
+        >
+          Write me <i class="bx bx-right-arrow-alt  underline" />
+        </a>
+      </div>
+    </div>
+    <!-- Telegram -->
+    <a
+      class="btn btn-circle bg-base-100"
+      href="https://t.me/jamalshr"
+      target="_blank"
+    >
+      <i class="text-blue-400 text-xl bx bxl-telegram" />
+    </a>
+    <a
+      class="btn btn-circle bg-base-100"
+      href="https://join.skype.com/invite/bOsjSN8qhcQh"
+      target="_blank"
+    >
+      <i class="text-blue-400 text-xl bx bxl-skype" />
+    </a>
+    <a
+      class="btn btn-circle bg-base-100  "
+      href="tel:+923001234567"
+      type="tel"
+      target="_blank"
+    >
+      <i class="text-green-400 text-xl bx bxl-whatsapp" />
+    </a>
+  </div>
+</section>
