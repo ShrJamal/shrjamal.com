6fddb1e53445b02cfe2c8e9a35b2699b91815492
Jamal .S <me@shrjamal.com>
2022-06-14 22:04:58 +0100

    change %svelte.head

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
-// import adapter from '@sveltejs/adapter-auto'
-import adapter from '@sveltejs/adapter-cloudflare'
+import adapter from '@sveltejs/adapter-auto'
 import preprocess from 'svelte-preprocess'
 import WindiCSS from 'vite-plugin-windicss'
 
