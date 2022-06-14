9a6b235b5724a9e5890c118d5d7f91d33d1af1a9
Jamal .S <me@shrjamal.com>
2022-06-14 21:47:22 +0100

    add cloudflare adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto'
+// import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-cloudflare'
 import preprocess from 'svelte-preprocess'
 import WindiCSS from 'vite-plugin-windicss'
 
