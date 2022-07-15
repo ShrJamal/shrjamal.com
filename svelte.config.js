c5e55cc0aa18f4138df0de799d3de30060d00637
Jamal .S <me@shrjamal.com>
2022-07-15 13:31:42 +0100

    migrate to new sveltekit version

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,11 @@
 import adapter from '@sveltejs/adapter-auto'
 import preprocess from 'svelte-preprocess'
-import WindiCSS from 'vite-plugin-windicss'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: [preprocess()],
+  preprocess: preprocess(),
   kit: {
     adapter: adapter(),
-    vite: () => ({
-      plugins: [WindiCSS()],
-    }),
   },
 }
 
