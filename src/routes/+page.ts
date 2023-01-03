16367e404c0fd0d0999eceefab6f382b34e07824
ShrJamal <me@shrjamal.com>
2023-01-03 21:32:10 +0100

    change background color

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,2 +1,8 @@
 export const ssr = false
 export const prerender = true
+
+// Ideas for the future
+// - Add Programming Languages in Skills
+// - Add Icons for Skills
+// - Better view for projects with description and tech stack
+// - Add Better font
