5fd33a2fa91385f1892ce77db57a1d9817dddd83
ShrJamal <me@shrjamal.com>
2023-01-03 18:15:31 +0100

    refactor eperience section

--- src/routes/experience/Experience.svelte
+++ src/routes/experience/Experience.svelte
@@ -1,36 +1,45 @@
 <script>
-  import { experiences } from './data'
+  const experiences = [
+    {
+      description:
+        'Top Rated freelancer on Upwork, worked with more 30+ clients. I have 100% positive reviews on Upwork.',
+      company: 'Upwork',
+      img: '/upwork.webp',
+      alt: 'Shrjamal Upwork Profile',
+      link: 'https://www.upwork.com/freelancers/shrjamal',
+      date: 'since 2022',
+    },
+    {
+      description: 'Level 1 Seller in Fiverr with 100% positive reviews.',
+      company: 'Fiverr',
+      img: '/fiverr.webp',
+      alt: 'Shrjamal Fiverr Profile',
+      link: 'https://www.fiverr.com/jamalshr',
+      date: 'since 2018',
+    },
+  ]
 </script>
 
-<section id="experience" class="flex flex-col px-4">
-  <h2 class="text-2xl font-bold text-primary text-center">Experience</h2>
-
-  <div class="flex flex-col">
+<section id="experience" class="mt-8 flex flex-col items-center px-4">
+  <h2 class="gradient-text text-4xl ">Experience</h2>
+  <div class="mt-4 grid sm:grid-cols-2 grid-cols-1 gap-4 ">
     {#each experiences as item}
-      <div class="w-full flex flex-row mt-4 bg-base-300 rounded">
+      <a
+        class="bg-glass w-80 flex flex-col rounded-xl p-2"
+        href={item.link}
+        target="_blank"
+        rel="noreferrer"
+      >
         <img
-          class="w-40 h-30 mr-4 rounded-l"
+          class="bg-white w-80 h-48 object-contain rounded-t-xl"
           width="160"
-          height=""
+          height="120"
           src={item.img}
           alt={item.alt}
           loading="lazy"
         />
-        <div class="flex flex-col pr-4 py-4 w-full">
-          <h3 class="flex-1 ">{item.description}</h3>
-          <div class="flex justify-between items-center">
-            <p class="text-2xs">{item.date}</p>
-            <a
-              class="link link-primary text-right"
-              href={item.link}
-              target="_blank"
-              rel="noreferrer"
-            >
-              Hire Me
-            </a>
-          </div>
-        </div>
-      </div>
+        <h3 class="mt-2 flex-1 ">{item.description}</h3>
+      </a>
     {/each}
   </div>
 </section>
