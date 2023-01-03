20c512c82f1d3a8a8f24ec1ddce30b973120be2f
ShrJamal <me@shrjamal.com>
2023-01-03 17:54:15 +0100

    refactor Intro section

--- src/routes/Intro/Intro.svelte
+++ src/routes/Intro/Intro.svelte
@@ -1,27 +1,17 @@
-<section class="mt-10" id="home">
-  <div class="flex flex-col justify-center ">
-    <div class="text-center flex flex-col items-center">
-      <span>Hello, I am </span>
-      <h1 class="text-6xl my-2 w-80 text-center ">
-        Jamal <span class="text-5xl">ASAHRAOUI</span>
-      </h1>
-      <h2 class="text-lg font-thin">
-        Self-taught Full Stack Javascript Developer
-      </h2>
-    </div>
-    <div class="flex my-10 justify-center">
-      <div
-        class="w-52 h-80 mx-2 flex items-end rounded-t-full rounded-b-2xl from-primary to-primary-focus bg-gradient-to-b"
-      >
-        <img
-          class="aspect-[2/3]"
-          src="/profile.webp"
-          alt="ShrJamal Profile"
-          loading="lazy"
-          width="208"
-          height="320"
-        />
-      </div>
-    </div>
-  </div>
+<section
+  id="home"
+  class="flex flex-col justify-center items-center text-primary-content"
+>
+  <img
+    class="mt-10 bg-glass w-72 aspect-square rounded-full"
+    src="/profile.webp"
+    alt="ShrJamal Profile"
+    loading="lazy"
+    width="288"
+    height="288"
+  />
+  <h1 class="mt-4 text-4xl text-center text-primary-content">
+    Jamal ASAHRAOUI
+  </h1>
+  <h2 class="text-lg font-thin ">Self-taught Full Stack Developer</h2>
 </section>
