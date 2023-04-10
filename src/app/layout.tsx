f7af03cd6383661a049ae6b1c53e6142e1617510
ShrJamal <me@shrjamal.com>
2023-04-10 19:45:12 +0000

    chore: add umami tracking script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Analytics } from '@vercel/analytics/react'
 import clsx from 'lib/clsx'
 
 import '~/global.css'
+import Script from 'next/script'
 
 type Props = {
   children: React.ReactNode
@@ -10,7 +11,12 @@ type Props = {
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
-      <head></head>
+      <head>
+        <Script
+          data-website-id="b59b48d7-8a5e-44ce-9fc5-d3966fb0dbff"
+          src="https://analytics.shrjamal.com/umami.js"
+        ></Script>
+      </head>
       <body className={clsx('h-screen flex text-primary-content')}>
         {children}
         <Analytics />
