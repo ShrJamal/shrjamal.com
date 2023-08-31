9f85f05e11f62b8ae4df5588ebb4b62f723a06c8
ShrJamal <me@shrjamal.com>
2023-08-31 13:01:52 +0100

    chore: upgrade astro v3

--- astro.config.js
+++ astro.config.js
@@ -6,7 +6,4 @@ import tailwind from '@astrojs/tailwind'
 export default defineConfig({
   site: 'https://shrjamal.com',
   integrations: [sitemap(), tailwind()],
-  experimental: {
-    assets: true,
-  },
 })
