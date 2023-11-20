8a9e925389af8537bc9507633f0cb81f8d55fc2d
Jamal Shr <me@shrjamal.com>
2023-11-20 20:58:55 +0100

    chore: upgrade deps and replace astro-icons

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,25 +1,31 @@
 const { fontFamily } = require('tailwindcss/defaultTheme')
-const themes = require('daisyui/src/theming/themes')
+const { addDynamicIconSelectors } = require('@iconify/tailwind')
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,jsx,tsx,astro,vue,svelte}'],
-  darkMode: ['class', '[data-theme="main"]'],
+  darkMode: ['class'],
+  content: ['./src/**/*.{html,js,jsx,ts,tsx,astro,vue,svelte,mdx}'],
   theme: {
     extend: {
       fontFamily: {
         sans: ['var(--font-inter)', ...fontFamily.sans],
       },
+      screens: {
+        xs: '475px',
+      },
     },
   },
-  plugins: [require('daisyui')],
+  plugins: [require('daisyui'), addDynamicIconSelectors()],
   daisyui: {
+    logs: false,
     themes: [
       {
         main: {
-          ...themes['[data-theme=dark]'],
+          ...require('daisyui/src/theming/themes')['dark'],
           primary: '#10aebd',
           secondary: '#42DEE1',
           accent: '#6DECB9',
+          'base-content': '#ffffff',
         },
       },
     ],
