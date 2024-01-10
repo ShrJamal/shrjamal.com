18a948aa085057dafb3ede8fde83e8e84884c320
Jamal Shr <me@shrjamal.com>
2024-01-10 22:08:02 +0100

    refactor

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 export const AppConfig = {
   site: 'https://shrjamal.com',
   site_name: 'Shrjamal',
-  author: 'Jamal Asahraoui',
   description:
     'I am Jamal Asahraoui, a FullStack web developer currently living in Morocco. My interests range from programming to entrepreneurship. Check my portfolio.',
   favicon: '/images/favicon.png',
