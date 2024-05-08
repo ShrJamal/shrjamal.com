1e045bac93528f2d9d94a1c57ac6c9e87a3d4d29
Jamal Shr <me@shrjamal.com>
2024-05-08 22:06:27 +0100

    remove .env.dev

--- astro.config.js
+++ astro.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config'
-import { loadEnv } from 'vite'
 import tailwind from '@astrojs/tailwind'
 import metaTags from 'astro-meta-tags'
 
-const env = loadEnv(process.env.NODE_ENV, process.cwd(), '')
-
 // https://astro.build/config
 export default defineConfig({
-  site: env.SITE,
+  site: 'https://cjamal.com',
   integrations: [tailwind(), metaTags()],
 })
