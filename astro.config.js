7d7db2a037d39fe50ef77238e80f6f809b026f83
Jamal Shr <shrjamal.dev@gmail.com>
2024-07-06 11:53:28 +0100

    cleanup

--- astro.config.js
+++ astro.config.js
@@ -11,4 +11,7 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
   ],
+  devToolbar: {
+    enabled: false,
+  },
 })
--- src/app.ts
+++
@@ -1,8 +0,0 @@
-export const AppConfig = {
-  site: import.meta.env.SITE,
-  site_name: 'Shrjamal',
-  title: 'Jamal Asahraoui - FullStack Web Developer',
-  description:
-    'I am Jamal Asahraoui, a FullStack web developer currently living in Morocco. My interests range from programming to entrepreneurship. Check my portfolio.',
-  twitter: '@shrjamal',
-}
