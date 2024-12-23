a54bd2ee9b9a4f59d3d00d235ef82991d5811dd8
Jamal Shr <shrjamal.dev@gmail.com>
2024-12-23 19:36:29 +0100

    refactor: update project techs to use constants for better maintainability

---
+++ src/lib/consts.ts
@@ -0,0 +1,102 @@
+export const techs = {
+  tailwindcss: {
+    name: "TailwindCSS",
+    icon: "icon-[logos--tailwindcss-icon]",
+    link: "https://tailwindcss.com",
+  },
+  typescript: {
+    name: "Typescript",
+    icon: "icon-[logos--typescript-icon]",
+    link: "https://typescriptlang.org",
+  },
+  astro: {
+    name: "Astro",
+    icon: "icon-[logos--astro-icon]",
+    link: "https://astro.build",
+  },
+  solidjs: {
+    name: "SolidJS",
+    icon: "icon-[logos--solidjs-icon]",
+    link: "https://solidjs.com",
+  },
+  react: {
+    name: "React",
+    icon: "icon-[logos--react]",
+    link: "https://reactjs.org",
+  },
+  svelte: {
+    name: "Svelte",
+    icon: "icon-[logos--svelte-icon]",
+    link: "https://svelte.dev",
+  },
+  nextjs: {
+    name: "NextJS",
+    icon: "icon-[logos--nextjs-icon]",
+    link: "https://nextjs.org",
+  },
+  nodejs: {
+    name: "NodeJS",
+    icon: "icon-[logos--nodejs-icon]",
+    link: "https://nodejs.org",
+  },
+  cloudflare: {
+    name: "Cloudflare",
+    icon: "icon-[logos--cloudflare-icon]",
+    link: "https://cloudflare.com",
+  },
+  firebase: {
+    name: "Firebase",
+    icon: "icon-[logos--firebase]",
+    link: "https://firebase.google.com",
+  },
+  supabase: {
+    name: "Supabase",
+    icon: "icon-[logos--supabase-icon]",
+    link: "https://supabase.com",
+  },
+  bootstrap: {
+    name: "Bootstrap",
+    icon: "icon-[logos--bootstrap]",
+    link: "https://getbootstrap.com",
+  },
+  materialui: {
+    name: "MaterialUI",
+    icon: "icon-[logos--material-ui]",
+    link: "https://mui.com",
+  },
+  leaflet: {
+    name: "Leaflet",
+    icon: "icon-[logos--leaflet]",
+    link: "https://leafletjs.com",
+  },
+  dart: {
+    name: "Dart",
+    icon: "icon-[logos--dart]",
+    link: "https://dart.dev",
+  },
+  flutter: {
+    name: "Flutter",
+    icon: "icon-[logos--flutter]",
+    link: "https://flutter.dev",
+  },
+  android: {
+    name: "Android",
+    icon: "icon-[logos--android-icon]",
+    link: "https://developer.android.com",
+  },
+  ios: {
+    name: "iOS",
+    icon: "icon-[logos--ios]",
+    link: "https://developer.apple.com/ios",
+  },
+  mongodb: {
+    name: "MongoDB",
+    icon: "icon-[logos--mongodb-icon]",
+    link: "https://mongodb.com",
+  },
+  postgresql: {
+    name: "PostgreSQL",
+    icon: "icon-[logos--postgresql]",
+    link: "https://postgresql.org",
+  },
+}
